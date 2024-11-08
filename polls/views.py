bbfa978f1a6405e2cd1c66916234b06630ec2beb
Sam Shearer <samshearer@Sams-MacBook-Pro.local>
2024-11-08 14:57:24 -0700

    samples

--- polls/views.py
+++ polls/views.py
@@ -4,3 +4,7 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
+
+
+def newMethod(request):
+    return HttpResponse("Our first app.")
