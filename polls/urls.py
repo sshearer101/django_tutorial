00de59f3bae2c7bb7bdd1600400970008415c822
Sam Shearer <sshearer101@gmail.com>
2025-01-30 12:51:45 -0700

    add html

--- polls/urls.py
+++ polls/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 
 from . import views
 
+app_name = "polls"
 urlpatterns = [
     path("", views.index, name="index"),
     path("first", views.newMethod, name="newMethod"),
--- polls/views.py
+++ polls/views.py
@@ -1,9 +1,14 @@
-from django.shortcuts import render
+from django.http import Http404
+from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponse
+
+from .models import Question
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    latest_question_list = Question.objects.order_by("-pub_date")[:5]
+    context = {"latest_question_list": latest_question_list}
+    return render(request, "polls/index.html", context)
 
 
 def newMethod(request):
@@ -11,7 +16,8 @@ def newMethod(request):
 
 
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/detail.html", {"question": question})
 
 
 def results(request, question_id):
